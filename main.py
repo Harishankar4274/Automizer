39384e68e99b33b56307891979adf2d3b4cbf5f2
Harishankar4274 <harishankardubey2911@gmail.com>
2020-08-07 02:25:09 +0530

    OS detection enabled

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import platform
 
 os.system("python3 -V")
 os.system("pip3 -V")
-os.system("pip3 install ansible")
+#os.system("pip3 install ansible")
+print(os.name)
+print(platform.system())
+print(platform.release())
